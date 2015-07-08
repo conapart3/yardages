084556f23f3867b97444b2a453940c24d218c52d
Conal <conalsmith49@gmail.com>
2015-07-08 03:25:48 +0100

    Updated to Material Design Library, enabled use of GPS provider.

--- app/src/main/java/com/yardages/YardagesActivity.java
+++ app/src/main/java/com/yardages/YardagesActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gc.materialdesign.views.ButtonRectangle;
+import com.gc.materialdesign.views.ButtonFloat;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -20,31 +22,45 @@ import android.location.LocationListener;
 /**
  * Created by Conal on 17/06/2015.
  */
-public class YardagesActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
+public class YardagesActivity extends Activity  {
 
     GoogleApiClient mGoogleApiClient;
     Location location1,location2;
     LocationManager locationManager;
     LocationListener locationListener;
-// Request code to use when launching the resolution activity
+    int changeCounts = 0;
+    // Request code to use when launching the resolution activity
 
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-    // Unique tag for the error dialog fragment
-    private static final String DIALOG_ERROR = "dialog_error";
-    // Bool to track whether the app is already resolving an error
-    private boolean mResolvingError = false;
+    private ButtonRectangle button1, button2, button3;
+    private TextView latitude1, longitude1, latitude2, longitude2, distance;
+    private ButtonFloat floatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.yardages_main);
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        button1 = (ButtonRectangle) findViewById(R.id.button1);
+        button2 = (ButtonRectangle) findViewById(R.id.button2);
+        button3 = (ButtonRectangle) findViewById(R.id.button3);
+        floatButton = (ButtonFloat) findViewById(R.id.buttonFloat);
+        latitude1 = (TextView) findViewById(R.id.latitude1);
+        longitude1 = (TextView) findViewById(R.id.longitude1);
+        latitude2 = (TextView) findViewById(R.id.latitude2);
+        longitude2 = (TextView) findViewById(R.id.longitude2);
+        distance = (TextView) findViewById(R.id.distance);
 
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         locationListener = new LocationListener(){
             public void onLocationChanged(Location location) {
+                changeCounts = changeCounts + 1;
                 Context context = getApplicationContext();
-                CharSequence text = "Location changed.";
+                CharSequence text = "Location changed." + changeCounts;
                 int duration = Toast.LENGTH_SHORT;
 
                 Toast toast = Toast.makeText(context, text, duration);
@@ -52,47 +68,27 @@ public class YardagesActivity extends Activity implements ConnectionCallbacks, O
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras){}
-
             public void onProviderEnabled(String provider) {}
-
             public void onProviderDisabled(String provider) {}
 
         };
 
-//        register listener with location manager to receive location updates.
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         setupButtonListeners();
-
-//        RecyclerView recycList = (RecyclerView) findViewById(R.id.cardList);
-//        recycList.setHasFixedSize(true);
-//        LinearLayoutManager llm = new LinearLayoutManager(this);
-//        llm.setOrientation(LinearLayoutManager.VERTICAL);
-//        recycList.setLayoutManager(llm);
-//        ClubCardAdapter ca = new ClubCardAdapter(createList(30));
-//        recycList.setAdapter(ca);
-
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
     private void setupButtonListeners(){
-        final Button button1 = (Button) findViewById(R.id.button1);
-        final Button button2 = (Button) findViewById(R.id.button2);
-        final Button button3 = (Button) findViewById(R.id.button3);
-
-        final TextView latitude1 = (TextView) findViewById(R.id.latitude1);
-        final TextView longitude1 = (TextView) findViewById(R.id.longitude1);
-
-        final TextView latitude2 = (TextView) findViewById(R.id.latitude2);
-        final TextView longitude2 = (TextView) findViewById(R.id.longitude2);
-
-        final TextView distance = (TextView) findViewById(R.id.distance);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                location1 = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                location1 = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
+                location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 if(location1 != null){
                     latitude1.setText(String.valueOf(location1.getLatitude()));
                     longitude1.setText(String.valueOf(location1.getLongitude()));
@@ -104,16 +100,13 @@ public class YardagesActivity extends Activity implements ConnectionCallbacks, O
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
                 }
-
             }
         });
-
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                location2 = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                location2 = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
+                location2 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 if (location2 != null) {
                     latitude2.setText(String.valueOf(location2.getLatitude()));
                     longitude2.setText(String.valueOf(location2.getLongitude()));
@@ -127,97 +120,39 @@ public class YardagesActivity extends Activity implements ConnectionCallbacks, O
                 }
             }
         });
-
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float dist = location1.distanceTo(location2);
-                distance.setText("" + dist);
+                CharSequence text="";
+                if(location1==null && location2==null)
+                    text = "Point A and B not set, wait for GPS signal.";
+                else if(location1==null && location2!=null)
+                    text = "Point A not set, select your start point.";
+                else if(location1!=null && location2==null)
+                    text = "Select Point B location.";
+
+                if(location1!=null && location2!=null){
+                    float dist = location1.distanceTo(location2);
+                    distance.setText("" + dist);
+                    text = "Distance shown.";
+                }
+
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence text = "Golf ball added.";
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
 
-//    private List<ClubCard> createList(int size) {
-//        List result = new ArrayList();
-//        for(int i=1;i<=size;i++){
-//            ClubCard cc = new ClubCard();
-//            cc.name = ClubCard.NAME_PREFIX + i;
-//            result.add(cc);
-//        }
-//        return result;
-//    }
-
-
-    @Override
-    public void onConnected(Bundle bundle) {
-// Connected to Google Play services!
-        // The good stuff goes here.
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-// The connection has been interrupted.
-        // Disable any UI components that depend on Google APIs
-        // until onConnected() is called.
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-    }
-
-//    private static final int TWO_MINUTES = 1000 * 60 * 2;
-//
-//    /** Determines whether one Location reading is better than the current Location fix
-//     * @param location  The new Location that you want to evaluate
-//     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
-//     */
-//    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-//        if (currentBestLocation == null) {
-//            // A new location is always better than no location
-//            return true;
-//        }
-//
-//        // Check whether the new location fix is newer or older
-//        long timeDelta = location.getTime() - currentBestLocation.getTime();
-//        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-//        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-//        boolean isNewer = timeDelta > 0;
-//
-//        // If it's been more than two minutes since the current location, use the new location
-//        // because the user has likely moved
-//        if (isSignificantlyNewer) {
-//            return true;
-//            // If the new location is more than two minutes older, it must be worse
-//        } else if (isSignificantlyOlder) {
-//            return false;
-//        }
-//
-//        // Check whether the new location fix is more or less accurate
-//        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-//        boolean isLessAccurate = accuracyDelta > 0;
-//        boolean isMoreAccurate = accuracyDelta < 0;
-//        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-//
-//        // Check if the old and new location are from the same provider
-//        boolean isFromSameProvider = isSameProvider(location.getProvider(),
-//                currentBestLocation.getProvider());
-//
-//        // Determine location quality using a combination of timeliness and accuracy
-//        if (isMoreAccurate) {
-//            return true;
-//        } else if (isNewer && !isLessAccurate) {
-//            return true;
-//        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    /** Checks whether two providers are the same */
-//    private boolean isSameProvider(String provider1, String provider2) {
-//        if (provider1 == null) {
-//            return provider2 == null;
-//        }
-//        return provider1.equals(provider2);
-//    }
 }
